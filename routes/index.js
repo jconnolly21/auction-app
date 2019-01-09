a67c362c761d0892100bcb480dd743457d35593b
Joseph Connolly <jconnolly@palantir.com>
2019-01-09 17:23:05 -0500

    maybe a fix

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-const { Pool } = require('pg');
-const pool = new Pool({
+const { Client } = require('pg');
+
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: true,
 });
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,17 +15,17 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get('/db', async function(req, res) {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM teams');
-      const results = { 'results': (result) ? result.rows : null};
-      res.send(results);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
+router.get('/db', function(req, res) {
+  client.connect();
+  
+  client.query('SELECT * FROM teams;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
     }
- });
+    client.end();
+  });
+  res.render('index', { title: 'success'});
+});
 
 module.exports = router;
