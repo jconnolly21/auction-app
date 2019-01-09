1c567c22c754390356d4238317c564640fc1358b
Joseph Connolly <jconnolly@palantir.com>
2019-01-09 18:55:42 -0500

    fix

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', index);
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
