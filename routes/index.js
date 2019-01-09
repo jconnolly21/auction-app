6ad3a424a018f150c5afad8e74c6bed776a4fd50
Joseph Connolly <jconnolly@palantir.com>
2019-01-09 16:59:22 -0500

    added db view

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM teams', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
-    });
-  });
-  //res.render('index', { title: 'Auction App' });
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Auction App' });
+// });
+
+
+router.get('/', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM teams');
+      const results = { 'results': (result) ? result.rows : null};
+      res.send(results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
 module.exports = router;
