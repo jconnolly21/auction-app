a820b3e63633df7fbd1b853444ccff41d8645341
Joseph Connolly <jconnolly@palantir.com>
2019-01-09 19:36:10 -0500

    blah

--- routes/index.js
+++ routes/index.js
@@ -11,16 +11,10 @@ const client = new Client({
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Auction App' });
-});
-
-
-router.get('/db', function(req, res) {
   client.connect();
-  var rows;
   client.query('SELECT * FROM teams;', (err, response) => {
-    if (err) throw err;
-    res.render('db', { title: 'Auction App', data: response.rows});
+  	if (err) throw err;
+    res.render('index', { title: 'Auction App', data: response.rows });
     client.end();
   });
 });
