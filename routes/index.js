b7df38d26d249e34857dcdb84ff3d0d86a275053
Joseph Connolly <jconnolly@palantir.com>
2019-01-10 12:12:36 -0500

    UI version 1

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-const { Client } = require('pg');
+// const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true,
+// });
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  client.connect();
-  client.query('SELECT * FROM teams;', (err, response) => {
-  	if (err) throw err;
-    res.render('index', { title: 'Auction App', data: response.rows });
-    client.end();
-  });
+  // client.connect();
+  // client.query('SELECT * FROM teams;', (err, response) => {
+  // 	if (err) throw err;
+  //   res.render('index', { title: 'Auction App', data: response.rows });
+  //   client.end();
+  // });
+  res.render('index_test');
 });
 
 module.exports = router;
