ccdd22d0cb7dc559140cbad739c0f9a60479aaf7
Joseph Connolly <jconnolly@palantir.com>
2019-01-10 12:44:16 -0500

    first attempt at db connect

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-// const { Client } = require('pg');
+const { Client } = require('pg');
 
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true,
-// });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // client.connect();
-  // client.query('SELECT * FROM teams;', (err, response) => {
-  // 	if (err) throw err;
-  //   res.render('index', { title: 'Auction App', data: response.rows });
-  //   client.end();
-  // });
-  res.render('index');
+  client.connect();
+  client.query('SELECT * FROM teams;', (err, response) => {
+  	if (err) throw err;
+    res.render('index', { data: response.rows });
+    client.end();
+  });
 });
 
 module.exports = router;
