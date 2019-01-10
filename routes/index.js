df2a1ec0f2005942b3769c652dd7c97afbd8b940
Joseph Connolly <jconnolly@palantir.com>
2019-01-10 14:20:40 -0500

    wat pg

--- routes/index.js
+++ routes/index.js
@@ -12,16 +12,22 @@ const client = new Client({
 /* GET home page. */
 router.get('/', function(req, res, next) {
   client.connect(function (err, client, done) {
-  	client.query('SELECT * FROM teams;', (err, response) => {
-  		if (err) {
-  			console.log('Error querying db.');
-  			throw err;
-  		} else {
-    		console.log('Succesfully queried db.')
-    		res.render('index', { data: response.rows });
-    	}
-    	client.end();
-  	});
+  	if (err) {
+  		console.log('Cannot connect to db.');
+  		throw err;
+  	} else {
+  		console.log('Successfully connected to db.')
+  		client.query('SELECT * FROM teams;', (err, response) => {
+  			if (err) {
+  				console.log('Error querying db.');
+  				throw err;
+  			} else {
+    			console.log('Succesfully queried db.')
+    			res.render('index', { data: response.rows });
+    		}
+    		client.end();
+  		});
+  	}
   });
 });
 
