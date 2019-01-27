279e39e5bbd3af918cd041eb83b4681652b3f404
Joseph Connolly <jconnolly@palantir.com>
2019-01-27 12:57:34 -0500

    table redesign

--- public/scripts/fbb_equations.js
+++ public/scripts/fbb_equations.js
@@ -1,61 +1,67 @@
 var hitterEconomy;
 var pitcherEconomy;
 
-var hittersDrafted;
-var pitchersDrafted;
-
 var hitterRVals;
 var pitcherRVals;
 
 var hitterPitcherSplit = .65;
 
-function initializeVars (rosters) {
-	hittersDrafted = 0;
-	pitchersDrafted = 0;
+var hitters = new Array(0);
+var pitchers = new Array(0);
+
+function initializeVars (data) {
 
 	hitterEconomy = teams.length*260*(hitterPitcherSplit);
 	pitcherEconomy = teams.length*260*(1-hitterPitcherSplit);
 
-	for (var i = 0; i < rosters.length; i++) {
-		for (var j = 0; j < rosters[i].length; j++) {
-			if (rosters[i][j].type == 'Hitter') {
-				hitterEconomy -= rosters[i][j].price;
-				hittersDrafted += 1;
-			} else {
-				pitcherEconomy -= rosters[i][j].price;
-				pitchersDrafted += 1;
-			}
+	for (var i = 0; i < data.length; i++) {
+		if (data[i].type = 'Hitter') {
+			addEWH(data[i]);
+			hitters.push(data[i]);
+		} else {
+			pitchers.push(data[i]);
 		}
 	}
 }
 
-function initializeHitterValues (hitters, rosters) {
-
-
+function calculateHitterValues () {
+	setHitterRVals();
+	setHitterCatVals();
+	hitters.sort(function (a,b) {
+		return b.value - a.value;
+	});
+	for (var i = 0; i < 10; i++) {
+		console.log(hitters[i].name + ': $' + hitters[i].value);
+	}
 }
 
-function initializePitcherValues (pitchers, rosters) {
+function calculatePitcherValues () {
 }
 
-function setHitterCatVals (hitters) {
+function setHitterCatVals () {
 	var poolVals = [0,0,0,0,0];
-	for (var i = 0; i < (140 - hittersDrafted); i++) {
+	for (var i = 0; i < (14 * teams.length); i++) {
 		poolVals[0] += hitters[i].stat1;
-		poolVals[1] += hitters[i].stat2;
+		poolVals[1] += hitters[i].ewh;
 		poolVals[2] += hitters[i].stat3;
 		poolVals[3] += hitters[i].stat4;
 		poolVals[4] += hitters[i].stat5;
 	}
 	for (var i = 0; i < hitters.length; i++) {
-		hitters[i].value1 = 
+		hitters[i].value1 = (hitterEconomy * .2) * (hitters[i].stat1 - hitterRVals[0]) / (poolVals[0] - (14*teams.length*hitterRVals[0]));
+		hitters[i].value2 = (hitterEconomy * .2) * (hitters[i].ewh - hitterRVals[1]) / (poolVals[1] - (14*teams.length*hitterRVals[1]));
+		hitters[i].value3 = (hitterEconomy * .2) * (hitters[i].stat3 - hitterRVals[2]) / (poolVals[2] - (14*teams.length*hitterRVals[2]));
+		hitters[i].value4 = (hitterEconomy * .2) * (hitters[i].stat4 - hitterRVals[3]) / (poolVals[3] - (14*teams.length*hitterRVals[3]));
+		hitters[i].value5 = (hitterEconomy * .2) * (hitters[i].stat5 - hitterRVals[4]) / (poolVals[4] - (14*teams.length*hitterRVals[4]));
+		hitters[i].value = hitters[i].value1 + hitters[i].value2 + hitters[i].value3 + hitters[i].value4 + hitters[i].value5;
 	}
 }
 
-function setHitterRVals (hitters) {
+function setHitterRVals () {
 	var tempRVals = [0,0,0,0,0];
 	for (var i = (140 - hittersDrafted); i < (154 - hittersDrafted); i++) {
 		tempRVals[0] += hitters[i].stat1;
-		tempRVals[1] += hitters[i].stat2;
+		tempRVals[1] += hitters[i].ewh;
 		tempRVals[2] += hitters[i].stat3;
 		tempRVals[3] += hitters[i].stat4;
 		tempRVals[4] += hitters[i].stat5;
@@ -67,8 +73,6 @@ function setHitterRVals (hitters) {
 	hitterRVals[4] = tempRVals[4] / 14;
 }
 
-function addEWH (hitters) {
-	for (var i = 0; i < hitters.length; i++) {
-		hitters[i].ewh = (hitters[i].countstat*hitters[i].stat2) - (hitters[i].countstat*.315)
-	}
+function addEWH (player) {
+	player.ewh = (player.countstat*player.stat2) - (player.countstat*.315)
 }
