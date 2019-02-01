665da7d9be1e9ac6a45ca848a8acd8f25f7014e8
Joseph Connolly <jconnolly@palantir.com>
2019-02-01 11:16:54 -0500

    attempting to redo pos switching

--- public/scripts/ui_functions.js
+++ public/scripts/ui_functions.js
@@ -300,7 +300,7 @@ function updateRosterTable(data) {
 					}
 				}
 			}
-			eligString += '</select>';
+			eligString += '</select><option>' + filledBy[i].rosterspot + '</option>';
 			var printName = filledBy[i].name;
 			if (printName.length > 15) {
 				printName = printName.split(' ')[0][0] + '.' + printName.substring(printName.indexOf(' '));
